ec5d5802753bb515ec5a80ff07996e54ccc59e05
RJDalefon <rjluengas@dalefon.mx>
2023-12-18 09:31:01 -0600

    Se agregaron los 6 botones faltantes y se les dio estilos.

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,81 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  Future<void> playSound (int soundNumber) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
+          child: Column(children: [
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.amberAccent
+              ),
               onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource('note1.wav'));
-              }, child: const Text('Tocar'),
+                playSound(1);
+              },
+              child: const Text('Tocar'),
             ),
-          ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.green
+              ),
+              onPressed: () async {
+                playSound(2);
+              },
+              child: const Text('Tocar'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.red
+              ),
+              onPressed: () async {
+                playSound(3);
+              },
+              child: const Text('Tocar'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.tealAccent
+              ),
+              onPressed: () async {
+                playSound(4);
+              },
+              child: const Text('Tocar'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.deepPurple
+              ),
+              onPressed: () async {
+                playSound(5);
+              },
+              child: const Text('Tocar'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.deepOrangeAccent
+              ),
+              onPressed: () async {
+                playSound(6);
+              },
+              child: const Text('Tocar'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue
+              ),
+              onPressed: () async {
+                playSound(7);
+              },
+              child: const Text('Tocar'),
+            ),
+          ]),
         ),
       ),
     );
