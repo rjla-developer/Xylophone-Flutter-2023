03bcf5a32e5ad32d515891045b8961ba9d49cf8e
RJ Luengas <robertojla8@gmail.com>
2023-12-18 21:46:45 -0600

    Se arreglaron los estilos faltantes para el diseño que se pidio

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(const XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
-  Future<void> playSound (int soundNumber) async {
+  Future<void> playSound(int soundNumber) async {
     final player = AudioPlayer();
     await player.play(AssetSource('note$soundNumber.wav'));
   }
@@ -15,72 +15,112 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(children: [
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.amberAccent
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.amberAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    playSound(1);
+                  },
+                  child: Container(),
+                ),
               ),
-              onPressed: () async {
-                playSound(1);
-              },
-              child: const Text('Tocar'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.green
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    playSound(2);
+                  },
+                  child: Container(),
+                ),
               ),
-              onPressed: () async {
-                playSound(2);
-              },
-              child: const Text('Tocar'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.red
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    playSound(3);
+                  },
+                  child: Container(),
+                ),
               ),
-              onPressed: () async {
-                playSound(3);
-              },
-              child: const Text('Tocar'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.tealAccent
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.tealAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    playSound(4);
+                  },
+                  child: Container(),
+                ),
               ),
-              onPressed: () async {
-                playSound(4);
-              },
-              child: const Text('Tocar'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.deepPurple
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.deepPurple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    playSound(5);
+                  },
+                  child: Container(),
+                ),
               ),
-              onPressed: () async {
-                playSound(5);
-              },
-              child: const Text('Tocar'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.deepOrangeAccent
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.deepOrangeAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    playSound(6);
+                  },
+                  child: Container(),
+                ),
               ),
-              onPressed: () async {
-                playSound(6);
-              },
-              child: const Text('Tocar'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    playSound(7);
+                  },
+                  child: Container(),
+                ),
               ),
-              onPressed: () async {
-                playSound(7);
-              },
-              child: const Text('Tocar'),
-            ),
-          ]),
+            ],
+          ),
         ),
       ),
     );
